2ac2b490caacf74da386b365429546d8bfc89d34
prathamt <prathamt@outlook.com>
2015-09-12 22:01:36 -0700

    Add Contact Form UI

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,6 +11,20 @@ class ContactForm extends React.Component {
         <div className='form-main'>
           <div className='form-div'>
             <h3 className='form-label'>Message Me!</h3>
+            <form className='form'>
+              <p className='name'>
+                <input id='name' className='feedback-input' type='text' placeholder='Name' name='name' required/>
+              </p>
+              <p className='email'>
+                <input id='email' className='feedback-input' type='email' placeholder='Email' name='email' required/>
+              </p>
+              <p className='text'>
+                <textarea id='comment' className='feedback-input' placeholder='Message' name='text'></textarea>
+              </p>
+              <div className='submit'>
+                <input id='button-red' type='submit' value='SEND'/>
+              </div>
+            </form>
           </div>
         </div>
       </section>
